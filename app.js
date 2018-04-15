384ec163a0810f4f89391d80b3fea67bb0d39b8b
Ramesh <rameshvishnoi90904@gmail.com>
2018-04-15 16:29:26 +0530

    added support for client

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var passport = require("passport");
+var request = require("request"),cors = require('cors');
 var Strategy = require("passport-twitter").Strategy;
 var OAuth = require("oauth").OAuth;
 const CONSUMER_KEY = "";
 const CONSUMER_SECRET = "";
-
+var router = express.Router();
 var oauth = new OAuth(
 	"https://api.twitter.com/oauth/request_token",
 	"https://api.twitter.com/oauth/access_token",
@@ -14,54 +15,14 @@ var oauth = new OAuth(
 	"CallBack URL",
 	"HMAC-SHA1"
 );
-var userSecretToken, userToken;
-// Configure the Twitter strategy for use by Passport.
-//
-// OAuth 1.0-based strategies require a `verify` function which receives the
-// credentials (`token` and `tokenSecret`) for accessing the Twitter API on the
-// user's behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(
-	new Strategy(
-		{
-			consumerKey: CONSUMER_KEY,
-			consumerSecret: CONSUMER_SECRET,
-			callbackURL: "http://localhost:3000/login/twitter/return"
-		},
-		function(token, tokenSecret, profile, cb) {
-			// In this example, the user's Twitter profile is supplied as the user
-			// record.  In a production-quality application, the Twitter profile should
-			// be associated with a user record in the application's database, which
-			// allows for account linking and authentication with other identity
-			// providers.
-			userToken = token;
-			userSecretToken = tokenSecret;
-			return cb(null, profile, token, tokenSecret);
-		}
-	)
-);
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
-passport.serializeUser(function(user, cb) {
-	cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-	cb(null, obj);
-});
-
-// Create a new Express application.
 var app = express();
-
+var corsOption = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+};
+app.use(cors(corsOption));
 // Configure view engine to render EJS templates.
 
 // Use application-level middleware for common functionality, including
@@ -87,30 +48,18 @@ app.get("/", function(req, res) {
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "application/json");
 	res.setHeader("Access-Control-Allow-Origin", "*");
-
-	// res.write(JSON.stringify(req.user));
 	res.end();
 });
 
-app.get("/login/twitter", passport.authenticate("twitter"));
 
-app.get(
-	"/login/twitter/return",
-	passport.authenticate("twitter", { failureRedirect: "/login" }),
-	function(req, res) {
-		res.redirect("/getTweetList");
-	}
-);
-
-app.get("/getTweetList/", function(req, res) {
+app.get("/getTweetList/",function(req, res) {
 	var url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-	oauth.get(url, userToken, userSecretToken, function(err, body, response) {
+	oauth.get(url, req.headers["clienttoken"], req.headers["clientsecrettoken"], function(err, body, response) {
 		if (!err && response.statusCode == 200) {
 			// success(body);
 		} else {
 			// error(err, response, body);
 		}
-
 		res.statusCode = 200;
 		res.setHeader("Content-Type", "application/json");
 		res.setHeader("Access-Control-Allow-Origin", "*");
@@ -119,8 +68,47 @@ app.get("/getTweetList/", function(req, res) {
 	});
 });
 
-// Twitter API end point to get recent 10 tweets from user time line. Replace screen_name value with twitter id.
+router.route('/auth/twitter/reverse')
+  .post(function(req, res) {
+    request.post({
+      url: 'https://api.twitter.com/oauth/request_token',
+      oauth: {
+        oauth_callback: "http://localhost:3000/",
+        consumer_key: CONSUMER_KEY,
+        consumer_secret: CONSUMER_SECRET
+      }
+    }, function (err, r, body) {
+      if (err) {
+        return res.send(500, { message: e.message });
+      }
+      var jsonStr = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+      res.send(JSON.parse(jsonStr));
+    });
+  });
 
-// Make a get request to twitter RESt API
+router.route('/auth/twitter')
+  .post(function(req, res, next) {
+    request.post({
+      url: `https://api.twitter.com/oauth/access_token?oauth_verifier`,
+      oauth: {
+        consumer_key: CONSUMER_KEY,
+        consumer_secret: CONSUMER_SECRET,
+        token: req.query.oauth_token
+      },
+      form: { oauth_verifier: req.query.oauth_verifier }
+    }, function (err, r, body) {
+      if (err) {
+        return res.send(500, { message: err.message });
+      }
 
-app.listen(3000);
+      const bodyString = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+      const parsedBody = JSON.parse(bodyString);
+      req.body['oauth_token'] = parsedBody.oauth_token;
+      req.body['oauth_token_secret'] = parsedBody.oauth_token_secret;
+      req.body['user_id'] = parsedBody.user_id;
+			return res.status(200).send(JSON.stringify(parsedBody));
+    });
+  });
+
+app.use('/api/v1', router);
+app.listen(3001);
